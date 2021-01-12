9462455eeed2128ae63eb2765f4f979f6933a37c
Paul Neenan <paulmneenan@gmail.com>
2021-01-12 17:04:00 -0500

    updated model for price

--- server/db/models/singleproduct.js
+++ server/db/models/singleproduct.js
@@ -11,7 +11,7 @@ module.exports = db.define('products', {
     }
   },
   price: {
-    type: Sequelize.FLOAT,
+    type: Sequelize.DECIMAL(25, 2),
     validate: {
       min: 0.01,
       notEmpty: true
