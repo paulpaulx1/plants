7ea15b922a4e4b284356d53c9b56db65b6d32ba9
Sung Lee <sungyonglee414@gmail.com>
2021-01-15 00:04:36 -0800

    feat/fix: updated react component with varies buttons(add,subtract,remove,checkout), price rounding fixed, cart on navbar included, updated redux for guestShoppingCart

--- client/store/guestShoppingCart.js
+++ client/store/guestShoppingCart.js
@@ -1,17 +1,27 @@
 const SET_GUEST_SHOPPING_CART = 'SET_GUEST_SHOPPING_CART'
 const ADD_PRODUCT_TO_CART = 'ADD_PRODUCT_TO_CART'
-
-// const ADD_PRODUCT = 'ADD_PRODUCT'
-// const SUBTRACT_PRODUCT = 'SUBTRACT_PRODUCT'
-// const DELETE_PRODUCT = 'DELETE_PRODUCT'
+const ADD_TO_CART = 'ADD_TO_CART'
+const SUBTRACT_FROM_CART = 'SUBTRACT_FROM_CART'
+const DELETE_FROM_CART = 'DELETE_FROM_CART'
 
 export const setGuestShoppingCart = products => ({
   type: SET_GUEST_SHOPPING_CART,
   products
 })
-
 export const addProductToCart = guestCart => ({
   type: ADD_PRODUCT_TO_CART,
+  guestCart
+})
+export const addToCart = guestCart => ({
+  type: ADD_TO_CART,
+  guestCart
+})
+export const subtractFromCart = guestCart => ({
+  type: SUBTRACT_FROM_CART,
+  guestCart
+})
+export const deleteFromCart = guestCart => ({
+  type: DELETE_FROM_CART,
   guestCart
 })
 
@@ -27,18 +37,16 @@ export const getGuestShoppingCart = () => {
 }
 
 export const addingToShoppingCart = product => {
-  console.log('product ----->', product)
-  console.log('localstorage ---->', localStorage)
   return dispatch => {
     try {
       let cart = JSON.parse(localStorage.getItem('shoppingCart'))
       if (cart) {
         const productArray = [...cart].map(item => item.id)
-        // if product is not in the cart
+        // checking id to see if product is not in the cart
         if (!productArray.includes(product.id)) {
           cart.push(product)
         } else {
-          console.log('cart', cart)
+          // if product is in the cart...loop through to increase quantity
           for (let i = 0; i < cart.length; i++) {
             if (product.id === cart[i].id) {
               cart[i].orderQuantity++
@@ -47,18 +55,65 @@ export const addingToShoppingCart = product => {
         }
       } else {
         cart = []
-
         cart.push(product)
       }
       localStorage.setItem('shoppingCart', JSON.stringify(cart))
       dispatch(addProductToCart(cart))
-      // localStorage.clear()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+export const addingToCart = productId => {
+  return dispatch => {
+    try {
+      let cart = JSON.parse(localStorage.getItem('shoppingCart'))
+      cart.filter(item => {
+        //filter through cart to see and increase the quantity of the product
+        if (productId === item.id) {
+          return (item.orderQuantity += 1)
+        }
+      })
+      localStorage.setItem('shoppingCart', JSON.stringify(cart))
+      dispatch(addToCart(cart))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+export const subtractingFromCart = productId => {
+  return dispatch => {
+    try {
+      let cart = JSON.parse(localStorage.getItem('shoppingCart'))
+      cart.filter(item => {
+        //filter through cart to see and decrease the quantity of the product
+        if (productId === item.id && item.orderQuantity !== 1) {
+          return (item.orderQuantity -= 1)
+        }
+      })
+      localStorage.setItem('shoppingCart', JSON.stringify(cart))
+      dispatch(subtractFromCart(cart))
     } catch (err) {
       console.log(err)
     }
   }
 }
 
+export const deletingFromCart = productId => {
+  return dispatch => {
+    try {
+      let cart = JSON.parse(localStorage.getItem('shoppingCart'))
+      let newCart = cart.filter(item => {
+        //filter through cart and return items that are not the product
+        if (productId !== item.id) return item
+      })
+      localStorage.setItem('shoppingCart', JSON.stringify(newCart))
+      dispatch(deleteFromCart(newCart))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
 const initialState = {
   cart: []
 }
@@ -68,6 +123,12 @@ export default function guestShoppingCartReducer(state = initialState, action) {
     case SET_GUEST_SHOPPING_CART:
       return {...state, cart: action.products}
     case ADD_PRODUCT_TO_CART:
+      return {...state, cart: action.guestCart}
+    case ADD_TO_CART:
+      return {...state, cart: action.guestCart}
+    case SUBTRACT_FROM_CART:
+      return {...state, cart: action.guestCart}
+    case DELETE_FROM_CART:
       return {...state, cart: action.guestCart}
     default:
       return state
---
+++ server/api/orders.js
@@ -0,0 +1,106 @@
+const router = require('express').Router()
+const {Product, OrderHistory, Order} = require('../db/models')
+module.exports = router
+
+//ensure it remains as put, increases and decreases quantity inside cart
+router.put('/:id/cart/:action', async function(req, res, next) {
+  try {
+    const {ProductId, OrderId} = req.body
+    const orderItem = await OrderHistory.findOne({
+      where: {
+        ProductId: ProductId,
+        OrderId: OrderId
+      }
+    })
+    if (req.params.action === 'increment') {
+      await orderItem.increment('quantity')
+    }
+    if (req.params.action === 'decrement') {
+      await orderItem.decrement('quantity')
+    }
+    res.json(orderItem)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//adds to the cart
+router.put('/:id/cart', async (req, res, next) => {
+  //ensure that front-end has proper naming convention for id being sent as "ProductId"
+  //ensure that front-end has proper naming convention for quantity being sent as "quantity"
+  try {
+    const currentProduct = await Product.findByPk(req.body.ProductId)
+    const currentOrder = await Order.findOne({
+      where: {UserId: req.params.id, processed: false}
+    })
+    await currentOrder.addProduct(currentProduct)
+    const newProduct = await Product.findByPk(req.body.ProductId, {
+      include: {
+        model: Order
+      }
+    })
+    const orderItem = await OrderHistory.findOne({
+      where: {
+        ProductId: req.body.ProductId,
+        OrderId: currentOrder.id
+      }
+    })
+    if (req.body.quantity) {
+      const newQuantity = req.body.quantity + orderItem.quantity - 1
+      await orderItem.update({quantity: newQuantity})
+    }
+    res.json(newProduct)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//(CART) remove product in cart
+router.delete('/:id/cart/:productId', async (req, res, next) => {
+  try {
+    const removedProduct = await Product.findByPk(req.params.productId)
+    const currentOrder = await Order.findOne({
+      where: {UserId: req.params.id, processed: false}
+    })
+    await currentOrder.removeProduct(removedProduct)
+    res.sendStatus(204)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//(CART) retrieve user cart
+router.get('/:id/cart', async (req, res, next) => {
+  try {
+    const cartItems = await Product.findAll({
+      include: {
+        model: Order,
+        where: {
+          UserId: req.params.id,
+          processed: false
+        }
+      }
+    })
+    if (cartItems.length === 0) {
+      const error = new Error('Your cart is empty')
+      next(error)
+    } else {
+      res.json(cartItems)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+//GET user order history
+router.get('/:id/orderHistory', async (req, res, next) => {
+  try {
+    const oldOrders = await Order.findAll({
+      where: {UserId: req.params.id, processed: true},
+      include: {model: Product}
+    })
+    res.json(oldOrders)
+  } catch (error) {
+    next(error)
+  }
+})
