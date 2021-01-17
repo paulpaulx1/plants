1b40a89d3b836915e6786fc0e5e2d505261c6701
Paul Neenan <paulmneenan@gmail.com>
2021-01-16 19:15:24 -0500

    feat: new css stuff

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,13 +6,13 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>CRAZY VITO'S HAT EMPORIUM</h1>
+    <h1 className="toplogo">CRAZY VITO'S HAT EMPORIUM</h1>
     <nav>
       {isLoggedIn ? (
         <div className="navstuff">
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
-          <Link to="/all">All Products</Link>
+          <Link to="/all">Hats</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
           <Link to="/guest/shoppingcart">
